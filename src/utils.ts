4fab43bc677933d21a41db5831b60312ac016fa8
Jackson Wong <108073827+wongjt22@users.noreply.github.com>
2025-02-24 20:29:02 +0000

    parse circular reference

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 const FLAT_VAL_URL = "https://bb676fcf-ab50-48cb-96c9-8dd0d467d56e-00-22g00du4q7t0s.spock.replit.dev"
 
-export async function evaluateCode(code: String): Promise<void> {
+export async function evaluateCode(code: string): Promise<string | undefined> {
 
   const payload = {
     code: code,
@@ -8,7 +8,7 @@ export async function evaluateCode(code: String): Promise<void> {
   };
 
   try {
-    const response = await fetch(FLAT_VAL_URL, {
+    const response = await fetch(FLAT_VAL_URL + "/eval", {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -21,9 +21,70 @@ export async function evaluateCode(code: String): Promise<void> {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    const result = await response.json();
-    console.log(result);
+    const responseData = await response.json();
+    const parsedResult = parseEvaluatedCode(responseData);
+    
+    return parsedResult
   } catch (error) {
     console.error('Request failed', error);
   }
+}
+
+interface SerializedItem {
+  type: 'string' | 'number | object' | 'array';
+  value: any;
+}
+
+interface SerializedResponse {
+  root: string;
+  serialized: {
+    [id: string]: SerializedItem;
+  };
+}
+
+export const parseEvaluatedCode = (response: SerializedResponse): string => {
+  const idToData = new Map<string, any>();
+
+  function parse(id: string): any {
+    if (idToData.has(id)) {
+      return idToData.get(id);
+    }
+
+    const item = response.serialized[id];
+    let data: any
+
+    switch (item.type) {
+      case 'number':
+        data = item.value;
+        break;
+      case 'string':
+        data = item.value;
+        break;
+      case 'object':
+        data = {};
+        idToData.set(id, data);
+        
+        for (const pair of item.value) {
+          const key = parse(pair.key);
+          const value = parse(pair.value);
+          data[key] = value;
+        }
+        break;
+      case 'array':
+        data = [];
+        idToData.set(id, data);
+
+        for (const elementId of item.value) {
+          data.push(parse(elementId));
+        }
+        break;
+      default:
+        throw new Error(`Unknown type: ${item.type}`);
+    }
+
+    idToData.set(id, data)
+    return data
+  }
+
+  return parse(response.root);
 }
